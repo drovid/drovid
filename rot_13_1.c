d497d8214dc92948b13ce258cd6840931de03dca
Edward Beknazarov <edbeknaz@e1z1r3p21.42.us.org>
2019-01-25 08:21:32 -0800

    first commit

---
+++ rot_13_1.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void		ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	rotate(char c)	
+{
+	if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
+		c += 13;
+	else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
+		c -= 13;
+	return (c);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 2)
+		while(*argv[1])
+			ft_putchar(rotate(*argv[1]++));
+	ft_putchar('\n');
+	return(0);
+}
+
